7bdb13c4c209e9f3e33a1fa3dae07fdd6de9f4e0
Melanie Benavides <melbenavides0@gmail.com>
2018-04-20 14:37:51 -0600

    Matriz coreciones en interfaz

---
+++ src/explodingteddies/cliente/vista/Tablero16x16Controller.java
@@ -0,0 +1,31 @@
+/*
+ * Programacion Concurrente Cliente Servidor
+ * 
+ * Melanie Benvides
+ * Jose Mora Loria
+ * Thomas White
+ * 
+ * Exploding Teddies
+ */
+package explodingteddies.cliente.vista;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author melbe
+ */
+public class Tablero16x16Controller implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
