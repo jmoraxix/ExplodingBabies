f061ade1a201f9f37c9d841a8e5770de65f96173
Jose Mora <jdevmora@gmail.com>
2018-04-20 17:28:11 -0600

    Creacion del controlador base de los tableros.

---
+++ src/explodingteddies/cliente/vista/TableroController.java
@@ -0,0 +1,38 @@
+/*
+ * Programacion Concurrente Cliente Servidor
+ * 
+ * Melanie Benvides
+ * Jose Mora Loria
+ * Thomas White
+ * 
+ * Exploding Teddies
+ */
+package explodingteddies.cliente.vista;
+
+import explodingteddies.cliente.Cliente;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author melbe
+ */
+public class TableroController implements Initializable {
+    
+    // Variables TCP
+    private Cliente cliente;
+    
+    // Variables juego
+    private ArrayListm
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
