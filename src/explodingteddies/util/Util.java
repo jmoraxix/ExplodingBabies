67a22a48ef4517a4ec9cff1a79c9984b41cb3569
Jose Mora <jdevmora@gmail.com>
2018-04-23 02:06:27 -0600

    Implementación Gson en utils.

--- src/explodingteddies/util/Util.java
+++ src/explodingteddies/util/Util.java
@@ -9,6 +9,8 @@
  */
 package explodingteddies.util;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import explodingteddies.modelo.Jugador;
 
 /**
@@ -16,7 +18,7 @@ import explodingteddies.modelo.Jugador;
  * @author josmora
  */
 public class Util {
-    
+
     //Variables de las ventanas
     public static int WIDTH = 650;
     public static int HEIGHT = 550;
@@ -24,7 +26,14 @@ public class Util {
     //Variables de TCP
     public static int SERVER_PORT = 2356;
     public static String SERVER_IP = "localhost";
-    
+
     //Variables globales de la aplicación
     public static Jugador CURRENT_USER = null;
+
+    private static Gson gson = new GsonBuilder().create();
+
+    public static Gson getGson() {
+        return gson;
+    }
+
 }
